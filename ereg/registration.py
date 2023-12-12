2ec487b32c308ad03dc9bd54d8a6051e24ffff95
sarthakpati <sarthak.pati@hotmail.com>
2023-12-12 15:18:15 -0500

    Add default optimizer parameters to registration class

--- ereg/registration.py
+++ ereg/registration.py
@@ -183,6 +183,49 @@ class RegistrationClass:
             "optimizer", "regular_step_gradient_descent"
         )
 
+        # this is taken directly from the sample_config.yaml
+        default_optimizer_parameters = {
+            "min_step": 1e-6, # regular_step_gradient_descent
+            "max_step": 1.0, # gradient_descent, regular_step_gradient_descent
+            "maximumStepSizeInPhysicalUnits": 1.0, # regular_step_gradient_descent, gradient_descent_line_search, gradient_descent,
+            "iterations": 1000, # regular_step_gradient_descent, gradient_descent_line_search, gradient_descent, conjugate, lbfgsb, lbfgsb2
+            "learningrate": 1.0, # gradient_descent, gradient_descent_line_search
+            "convergence_minimum": 1e-6, # gradient_descent, gradient_descent_line_search
+            "convergence_window_size": 10, # gradient_descent, gradient_descent_line_search
+            "line_search_lower_limit": 0.0, # gradient_descent_line_search
+            "line_search_upper_limit": 5.0, # gradient_descent_line_search
+            "line_search_epsilon": 0.01, # gradient_descent_line_search
+            "step_length": 0.1, # conjugate, exhaustive, powell
+            "simplex_delta": 0.1, # amoeba
+            "maximum_number_of_corrections": 5, # lbfgsb, lbfgsb2
+            "maximum_number_of_function_evaluations": 2000, # lbfgsb, lbfgsb2
+            "solution_accuracy": 1e-5, # lbfgsb2
+            "hessian_approximate_accuracy": 1e-5, # lbfgsb2
+            "delta_convergence_distance": 1e-5, # lbfgsb2
+            "delta_convergence_tolerance": 1e-5, # lbfgsb2
+            "line_search_maximum_evaluations": 50, # lbfgsb2
+            "line_search_minimum_step": 1e-20, # lbfgsb2
+            "line_search_accuracy": 1e-4, # lbfgsb2
+            "epsilon": 1e-8, # one_plus_one_evolutionary
+            "initial_radius": 1.0, # one_plus_one_evolutionary
+            "growth_factor": -1.0, # one_plus_one_evolutionary
+            "shrink_factor": -1.0, # one_plus_one_evolutionary
+            "maximum_line_iterations": 100, # powell
+            "step_tolerance": 1e-6, # powell
+            "value_tolerance": 1e-6, # powell
+        }
+
+        # check for optimizer parameters in config file
+        self.parameters["optimizer_parameters"] = self.parameters.get(
+            "optimizer_parameters", {}
+        )
+
+        # for any optimizer parameters not in the config file, use the default values
+        for key, value in default_optimizer_parameters.items():
+            if key not in self.parameters["optimizer_parameters"]:
+                self.parameters["optimizer_parameters"][key] = value
+
+
     def register(
         self,
         target_image: Union[str, sitk.Image],
