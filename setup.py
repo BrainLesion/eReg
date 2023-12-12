649eb1ffbc35ac0de5dc8a61e136d7c0c76a6b7a
neuronflow <florian.kofler@tum.de>
2023-12-12 02:24:47 +0100

    new: add requirements to toml, comment setup
    Signed-off-by: neuronflow <florian.kofler@tum.de>

--- setup.py
+++ setup.py
@@ -1,71 +1,71 @@
-#!/usr/bin/env python
+# #!/usr/bin/env python
 
-"""The setup script."""
+# """The setup script."""
 
 
-import sys, re
-from setuptools import setup, find_packages
+# import sys, re
+# from setuptools import setup, find_packages
 
-try:
-    with open("README.md") as readme_file:
-        readme = readme_file.read()
-except Exception as error:
-    readme = "No README information found."
-    sys.stderr.write("Warning: Could not open '%s' due %s\n" % ("README.md", error))
+# try:
+#     with open("README.md") as readme_file:
+#         readme = readme_file.read()
+# except Exception as error:
+#     readme = "No README information found."
+#     sys.stderr.write("Warning: Could not open '%s' due %s\n" % ("README.md", error))
 
-try:
-    filepath = "eReg/version.py"
-    version_file = open(filepath)
-    (__version__,) = re.findall('__version__ = "(.*)"', version_file.read())
+# try:
+#     filepath = "eReg/version.py"
+#     version_file = open(filepath)
+#     (__version__,) = re.findall('__version__ = "(.*)"', version_file.read())
 
-except Exception as error:
-    __version__ = "0.0.1"
-    sys.stderr.write("Warning: Could not open '%s' due %s\n" % (filepath, error))
+# except Exception as error:
+#     __version__ = "0.0.1"
+#     sys.stderr.write("Warning: Could not open '%s' due %s\n" % (filepath, error))
 
-requirements = [
-    "black",
-    "numpy==1.22.0",
-    "pyyaml",
-    "pytest",
-    "pytest-cov",
-    "requests",
-    "SimpleITK!=2.0.*",
-    "SimpleITK!=2.2.1",  # https://github.com/mlcommons/GaNDLF/issues/536
-    "scikit-image",
-    "setuptools",
-    "tqdm",
-]
+# requirements = [
+#     "black",
+#     "numpy==1.22.0",
+#     "pyyaml",
+#     "pytest",
+#     "pytest-cov",
+#     "requests",
+#     "SimpleITK!=2.0.*",
+#     "SimpleITK!=2.2.1",  # https://github.com/mlcommons/GaNDLF/issues/536
+#     "scikit-image",
+#     "setuptools",
+#     "tqdm",
+# ]
 
-if __name__ == "__main__":
-    setup(
-        name="eReg",
-        version=__version__,
-        author="FETS-AI",
-        author_email="admin@fets.ai",
-        python_requires=">=3.8",
-        packages=find_packages(),
-        entry_points={
-            "console_scripts": [
-                "register=eReg.cli.run:main",
-            ],
-        },
-        classifiers=[
-            "Development Status :: 1 - Planning",
-            "Intended Audience :: Science/Research",
-            "License :: OSI Approved :: Apache Software License",
-            "Natural Language :: English",
-            "Operating System :: OS Independent",
-            "Programming Language :: Python :: 3.8",
-            "Programming Language :: Python :: 3.9",
-            "Programming Language :: Python :: 3.10",
-            "Topic :: Scientific/Engineering :: Medical Science Apps.",
-        ],
-        description=("Template python project."),
-        install_requires=requirements,
-        license="Apache-2.0",
-        long_description=readme,
-        long_description_content_type="text/markdown",
-        include_package_data=True,
-        keywords="machine learning",
-        zip_safe=False,
-    )
+# if __name__ == "__main__":
+#     setup(
+#         name="eReg",
+#         version=__version__,
+#         author="FETS-AI",
+#         author_email="admin@fets.ai",
+#         python_requires=">=3.8",
+#         packages=find_packages(),
+#         entry_points={
+#             "console_scripts": [
+#                 "register=eReg.cli.run:main",
+#             ],
+#         },
+#         classifiers=[
+#             "Development Status :: 1 - Planning",
+#             "Intended Audience :: Science/Research",
+#             "License :: OSI Approved :: Apache Software License",
+#             "Natural Language :: English",
+#             "Operating System :: OS Independent",
+#             "Programming Language :: Python :: 3.8",
+#             "Programming Language :: Python :: 3.9",
+#             "Programming Language :: Python :: 3.10",
+#             "Topic :: Scientific/Engineering :: Medical Science Apps.",
+#         ],
+#         description=("Template python project."),
+#         install_requires=requirements,
+#         license="Apache-2.0",
+#         long_description=readme,
+#         long_description_content_type="text/markdown",
+#         include_package_data=True,
+#         keywords="machine learning",
+#         zip_safe=False,
+#     )
