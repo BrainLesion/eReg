14c968f3c2c381f38a39f9a6e2617f7c8b283033
neuronflow <florian.kofler@tum.de>
2023-12-12 02:38:36 +0100

    heimat
    Signed-off-by: neuronflow <florian.kofler@tum.de>

--- ereg/cli/run.py
+++ ereg/cli/run.py
@@ -7,6 +7,7 @@ from tqdm import tqdm
 
 # TODO we need to fix this version thing in a different way
 # from ereg import RegistrationClass, __version__
+from ereg import RegistrationClass
 from ereg.utilities import get_ssim
 
 
