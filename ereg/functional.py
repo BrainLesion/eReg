ec0a807176a7b3502e7e17de7251699f6eeeb9b6
sarthakpati <sarthak.pati@hotmail.com>
2024-03-30 13:45:21 -0400

    updated docstring comments

--- ereg/functional.py
+++ ereg/functional.py
@@ -10,7 +10,7 @@ def registration_function(
     target_image: Union[str, sitk.Image],
     moving_image: Union[str, sitk.Image],
     output_image: str,
-    configuration: str = None,
+    configuration: Union[str, dict] = None,
     transform_file: str = None,
     log_file: str = None,
     **kwargs,
@@ -22,7 +22,7 @@ def registration_function(
         target_image (Union[str, sitk.Image]): The target image.
         moving_image (Union[str, sitk.Image]): The moving image.
         output_image (str): The output image.
-        config_file (str, optional): The config file for the registration. Defaults to None.
+        configuration (Union[str, dict], optional): The configuration file or dictionary. Defaults to None.
         transform_file (str, optional): The transform file. Defaults to None.
 
     Returns:
